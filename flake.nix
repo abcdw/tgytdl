e34195d44f3c6a95a58624920bf50224ef10b464
Andrew Tropin <andrew@trop.in>
2020-08-25 18:37:09 +0300

    Add tennis-data to inputs

--- flake.nix
+++ flake.nix
@@ -3,18 +3,25 @@
   inputs = {
     stable.url = "github:NixOS/nixpkgs/nixos-20.03";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    tennis-data = {
+      url = "github:JeffSackmann/tennis_atp";
+      flake = false;
+    };
   };
 
   outputs = inputs:
     let
       system = "x86_64-linux";
       pkgs = inputs.unstable.legacyPackages.${system};
-      app = pkgs.poetry2nix.mkPoetryApplication { projectDir = ./.; };
+      # app = pkgs.poetry2nix.mkPoetryApplication { projectDir = ./.; };
       env = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
     in {
       devShell."${system}" = pkgs.mkShell {
-        buildInputs = [ env pkgs.gnumake ];
+        buildInputs = with pkgs; [ env ];
         PGPORT = 8000;
+        shellHook = ''
+          ln -snf ${inputs.tennis-data}/atp_matches_2020.csv dataset.csv
+        '';
       };
       # devShell."${system}" = import ./shell.nix {
       #   pkgs = inputs.stable.legacyPackages.${system};
