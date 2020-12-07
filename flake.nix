44ac3a168c21b1f7d927d3f67021b64e812afff9
Andrew Tropin <andrew@trop.in>
2020-12-07 13:31:49 +0300

    Update .envrc and reorganize flake.nix

--- flake.nix
+++ flake.nix
@@ -7,14 +7,46 @@
 
   outputs = inputs:
     let
+      lib = inputs.stable.lib;
       system = "x86_64-linux";
-      pkgs = inputs.unstable.legacyPackages.${system};
-      app = pkgs.poetry2nix.mkPoetryApplication { projectDir = ./.; };
-      env = pkgs.poetry2nix.mkPoetryEnv { projectDir = ./.; };
+      unstable-pkgs = inputs.unstable.legacyPackages.${system};
+      pkgs = inputs.stable.legacyPackages.${system};
+      runtimeDeps = with pkgs; [ youtube-dl ];
+      config = {
+        projectDir = ./.;
+        propagatedBuildInputs = runtimeDeps;
+      };
+      app = unstable-pkgs.poetry2nix.mkPoetryApplication config;
+      env = app.dependencyEnv; # pkgs.poetry2nix.mkPoetryEnv config;
     in {
       devShell."${system}" = pkgs.mkShell {
-        buildInputs = with pkgs; [ env poetry youtube-dl ];
+        buildInputs = with pkgs; [ env poetry nix-deploy ] ++ runtimeDeps;
       };
       defaultPackage."${system}" = app;
+
+      nixosConfigurations = {
+        aws-tgytdl = lib.nixosSystem {
+          inherit system;
+          modules = [{
+            imports = [
+              "${inputs.stable}/nixos/modules/virtualisation/amazon-image.nix"
+            ];
+            ec2.hvm = true;
+            networking.hostName = "aws-tgytdl";
+            systemd.services.tgytdl = {
+              description = "Telegram Bot for downloading youtube videos";
+              # path = [ pkgs.alsaTools ];
+              wantedBy = [ "default.target" ];
+              script = ''
+                ${inputs.self.defaultPackage."${system}"}/bin/tgytdl_CONTROL 0x0
+              '';
+            };
+            environment.systemPackages =
+              [ inputs.self.defaultPackage."${system}" ];
+          }];
+        };
+      };
+      aws-tgytdl =
+        inputs.self.nixosConfigurations.aws-tgytdl.config.system.build.toplevel;
     };
 }
